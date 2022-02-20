a2a1e50816ffc404333cabc9bbe7b4dc56da126c
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-02-20 22:24:21 +0100

    Create tostring.js

---
+++ tostring.js
@@ -0,0 +1,12 @@
+// We need a function that can transform a number into a string.
+//
+// What ways of achieving this do you know?
+//
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
