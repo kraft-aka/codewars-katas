b11df211bb11e10a4a3b13e6e94b96c10cc60391
kubat <kubat.aiupov@gmail.com>
2023-04-11 21:42:32 +0200

    add noOdds

---
+++ noOdds.js
@@ -0,0 +1,8 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order
+// they are given.
+
+function noOdds(values) {
+  return values.filter((value) => value % 2 === 0);
+}
