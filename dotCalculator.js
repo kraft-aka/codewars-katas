9a30891bd803b626a6bb34fc4f5a30b1efec5193
kubat <kubat.aiupov@gmail.com>
2023-04-12 22:16:02 +0200

    add dotCalculator

---
+++ dotCalculator.js
@@ -0,0 +1,47 @@
+// Dot Calculator
+// You have to write a calculator that receives strings for input. 
+// The dots will represent the number in the equation. 
+// There will be dots on one side, an operator, and dots again after the operator. 
+// The dots and the operator will be separated by one space.
+
+// Here are the following valid operators :
+
+// + Addition
+// - Subtraction
+// * Multiplication
+// // Integer Division
+// Your Work (Task)
+// You'll have to return a string that contains dots, as many the equation returns. 
+// If the result is 0, return the empty string. When it comes to subtraction, 
+// the first number will always be greater than or equal to the second number.
+
+// Examples (Input => Output)
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
+
+
+function dotCalculator (equation) {
+  const strSplit = equation.split(" ")
+  const firstPart = strSplit[0]
+  const secondPart = strSplit[2]
+  const operator= strSplit[1]
+  if(operator == "+") {
+    return firstPart + secondPart
+  }
+  else if(operator == "-") {
+    return ".".repeat(firstPart.length - secondPart.length)
+  }
+  else if(operator == "*") {
+    return ".".repeat(firstPart.length * secondPart.length)
+  }
+  else {
+    return ".".repeat(firstPart.length / secondPart.length)
+  }
+}
