5f7d545a847454359883d28e7ac079e60cbbf355
kubat <kubat.aiupov@gmail.com>
2023-07-25 20:20:06 +0200

    add twoArePositive

---
+++ twoArePositive.js
@@ -0,0 +1,20 @@
+// Task:
+// Your job is to write a function, which takes three
+// integers a, b, and c as arguments, and returns
+// True if exactly two of of the three integers are
+// positive numbers (greater than zero), and False - otherwise.
+
+// Examples:
+// twoArePositive(2, 4, -3) == true
+// twoArePositive(-4, 6, 8) == true
+// twoArePositive(4, -6, 9) == true
+// twoArePositive(-4, 6, 0) == false
+// twoArePositive(4, 6, 10) == false
+// twoArePositive(-14, -3, -4) == false
+
+function twoArePositive(a, b, c) {
+  if (a > 0 && b > 0 && c <= 0) return true;
+  if (a <= 0 && b > 0 && c > 0) return true;
+  if (a > 0 && b <= 0 && c > 0) return true;
+  return false;
+}
