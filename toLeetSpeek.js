792d94d3971ddd3087295367ba5f2cf10101202e
kubat <kubat.aiupov@gmail.com>
2023-07-29 23:19:00 +0200

    add toLeetSpeak

---
+++ toLeetSpeek.js
@@ -0,0 +1,52 @@
+// Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+// More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+// For example:
+
+// toLeetSpeak("LEET") returns "1337"
+
+function toLeetSpeak(str) {
+  const leet = {
+    A: "@",
+    B: "8",
+    C: "(",
+    D: "D",
+    E: "3",
+    F: "F",
+    G: "6",
+    H: "#",
+    I: "!",
+    J: "J",
+    K: "K",
+    L: "1",
+    M: "M",
+    N: "N",
+    O: "0",
+    P: "P",
+    Q: "Q",
+    R: "R",
+    S: "$",
+    T: "7",
+    U: "U",
+    V: "V",
+    W: "W",
+    X: "X",
+    Y: "Y",
+    Z: "2",
+  };
+  let newStr = "";
+  for (let val in str) {
+    if (str[val] in leet) {
+      newStr += leet[str[val]];
+    } else {
+      newStr += str[val];
+    }
+  }
+  return newStr;
+}
+
+
+console.log(toLeetSpeak('HELLO MAN!'))
