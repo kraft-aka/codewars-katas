890a3727e22172126e0394b42f2a7657cbf25feb
kubat <kubat.aiupov@gmail.com>
2023-07-29 23:25:04 +0200

    add getMinMax

---
+++ getMinMax.js
@@ -0,0 +1,7 @@
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+function getMinMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  return [min, max];
+}
