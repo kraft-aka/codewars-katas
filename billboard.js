96851d3aad0d4e7a3f5fe28d90fd77faafbbe8ca
kraft-aka <kubat.aiupov@gmail.com>
2023-08-18 17:12:06 +0200

    edit code

---
+++ billboard.js
@@ -0,0 +1,13 @@
+// You can print your name on a billboard ad. Find out how much it will cost you.
+// Each character has a default price of £30, but that can be different if you are
+// given 2 parameters instead of 1 (allways 2 for Java).
+
+// You can not use multiplier "*" operator.
+
+// If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
+
+const billboard = (name, price = 30) =>
+  name
+    .split("")
+    .map((i) => +price)
+    .reduce((a, b) => a + b, 0);
