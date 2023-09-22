2daf8f7c1b8197d6ce571240f80e6051616c8359
kubat <kubat.aiupov@gmail.com>
2023-09-22 20:08:13 +0200

    add dadFilter

---
+++ dadFilter.js
@@ -0,0 +1,12 @@
+// Your dad doesn't really get punctuation, and he's always putting extra commas in when he posts. Y
+// ou can tolerate the run-on sentences, as he does actually talk like that, but those extra commas have to go!
+
+// Write a function called dadFilter that takes a string as argument and returns a string with the extraneous commas removed.
+// The returned string should not end with a comma or any trailing whitespace.
+
+function dadFilter(str) {
+  return str
+    .replace(/^[\s,]+/, "")
+    .replace(/[\s,]+$/, "")
+    .replace(/\s*,+\s*(,+\s*)*/g, ", ");
+}
