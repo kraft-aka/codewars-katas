8d56e1e2c70b664cbc20b4d962afbaadeedb5a48
kubat <kubat.aiupov@gmail.com>
2023-09-26 22:41:18 +0200

    add solutionStringFromHash

---
+++ solutionStringsFromHash.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Complete the solution so that it takes the object (JavaScript/CoffeeScript) or
+// hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+function solution(pairs) {
+  // TODO: complete
+  let str = "";
+  for (const [k, v] of Object.entries(pairs)) {
+    str += `${k} = ${v},`;
+  }
+
+  return str.slice(0, -1);
+}
