a0e9e484d4baf3c95c6577209c6754b299377ec8
kubat <kubat.aiupov@gmail.com>
2023-10-02 22:48:39 +0200

    add longestWord

---
+++ longestWord.js
@@ -0,0 +1,20 @@
+// When given a string of space separated words,
+// return the word with the longest length.
+// If there are multiple words with the longest length,
+// return the last instance of the word with the longest length.
+
+// Example:
+
+// 'red white blue' //returns string value of white
+
+// 'red blue gold' //returns gold
+
+function longestWord(str) {
+  return (
+    str,
+    str
+      .split(" ")
+      .sort((a, b) => a.length - b.length)
+      .pop()
+  );
+}
