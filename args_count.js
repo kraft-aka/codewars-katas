ba54472b1097e161767bb38ee79753a03cf06dba
kubat <kubat.aiupov@gmail.com>
2023-10-13 21:30:04 +0200

    add args_count

---
+++ args_count.js
@@ -0,0 +1,7 @@
+// Write a function that returns the number of arguments it received.
+
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+const args_count = (...args) => args.length;
