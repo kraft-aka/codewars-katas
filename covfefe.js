473e840fc96caded7672ca9b2ce4579e03b8f884
kubat <kubat.aiupov@gmail.com>
2023-10-20 23:36:33 +0200

    add covfefe

---
+++ covfefe.js
@@ -0,0 +1,9 @@
+// Your are given a string. You must replace any occurence of the sequence coverage by covfefe, however,
+// if you don't find the word coverage in the string, you must add covfefe at the end of the string with a leading space.
+
+// For the languages where the string is mutable (such as ruby), don't modify the given string, otherwise this will break the test cases.
+
+const covfefe = (str) =>
+  str.includes("coverage")
+    ? str.replace(/coverage/g, "covfefe")
+    : str + " " + "covfefe";
