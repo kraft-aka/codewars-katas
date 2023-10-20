7b71ecd778b967bac56077b6b51aa07c50ea013e
kubat <kubat.aiupov@gmail.com>
2023-10-20 23:34:47 +0200

    add convertHashToArray

---
+++ convertHashToArray.js
@@ -0,0 +1,14 @@
+// Convert a hash into an array. Nothing more, Nothing less.
+
+// {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+// should be converted into
+
+// [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+// Note: The output array should be sorted alphabetically by key name.
+
+// Good Luck!
+
+const convertHashToArray = (hash) =>
+  Object.keys(hash)
+    .sort()
+    .map((k) => [k, hash[k]]);
