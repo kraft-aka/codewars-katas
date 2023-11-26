5c3467d8ec0963e9366ed59d99ef8da0e3216f11
kubat <kubat.aiupov@gmail.com>
2023-11-26 20:10:57 +0100

    add uniqueSum.js

---
+++ uniqueSum.js
@@ -0,0 +1,17 @@
+// Given a list of integers values, your job is to return the sum of the values; however,
+// if the same integer value appears multiple times in the list, you can only count
+// it once in your sum.
+
+// For example:
+
+// [ 1, 2, 3] ==> 6
+
+// [ 1, 3, 8, 1, 8] ==> 12
+
+// [ -1, -1, 5, 2, -7] ==> -1
+
+// [] ==> null
+
+function uniqueSum(lst) {
+  return lst.length === 0 ? null : [...new Set(lst)].reduce((a, b) => a + b, 0);
+}
