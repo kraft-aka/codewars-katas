81f8b6589ed36ecd37977f17882fcdb7a82f37e9
kubat <kubat.aiupov@gmail.com>
2023-12-04 23:21:08 +0100

    add repeats

---
+++ repeats.js
@@ -0,0 +1,12 @@
+// In this Kata, you will be given an array of numbers in which two numbers occur
+// once and the rest occur only twice.
+// Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8
+// occur once, and their sum is 15. Every other number occurs twice.
+
+function repeats(arr) {
+  return arr
+    .filter((i) => arr.indexOf(i) === arr.lastIndexOf(i))
+    .reduce((a, b) => a + b, 0);
+}
