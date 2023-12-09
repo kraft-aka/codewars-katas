e9dc1abf5591c783ee247828df25aefb111a95fa
kubat <kubat.aiupov@gmail.com>
2023-12-09 20:46:45 +0100

    add splitSentence

---
+++ splitSentence.js
@@ -0,0 +1,7 @@
+// Write function splitSentence which will create a list of strings from a string.
+
+// Example:
+
+// "hello world" -> ["hello", "world"]
+
+const splitSentence = (s) => s.split(" ");
