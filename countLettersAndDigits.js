0b4add13e9ed534f679ae10619e50bbc0884c2ae
kubat <kubat.aiupov@gmail.com>
2023-12-18 23:11:57 +0100

    add countLettersAndDigits

---
+++ countLettersAndDigits.js
@@ -0,0 +1,14 @@
+// Bob is a lazy man.
+
+// He needs you to create a method that can determine how many letters (both uppercase and lowercase ASCII letters) and digits are in a given string.
+
+// Example:
+
+// "hel2!lo" --> 6
+
+// "wicked .. !" --> 6
+
+// "!?..A" --> 1
+
+const countLettersAndDigits = (input) =>
+  input.replace(/[^a-zA-Z0-9]/g, "").length;
