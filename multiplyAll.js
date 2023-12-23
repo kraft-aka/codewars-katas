39cb719f278973229862ace6d658aca33e72c111
kubat <kubat.aiupov@gmail.com>
2023-12-23 22:07:18 +0100

    add multiplyAll

---
+++ multiplyAll.js
@@ -0,0 +1,22 @@
+// To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument.
+// This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+
+// Here's a nice Youtube video about currying, which might help you if this is new to you.
+
+function multiplyAll(arr) {
+  return function curriedFunction(multiplier) {
+    if (multiplier !== undefined) {
+      return arr.map(function (element) {
+        return element * multiplier;
+      });
+    }
+    return curriedFunction;
+  };
+}
