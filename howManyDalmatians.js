6b644aefe0d316ddf4df8182e38a6aa9f6eca575
kubat <kubat.aiupov@gmail.com>
2024-01-18 15:45:21 +0100

    add howManyDalmatians

---
+++ howManyDalmatians.js
@@ -0,0 +1,19 @@
+// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs,
+// and you simply do not know how to respond!
+
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+// The number of dogs will always be a number and there will always be at least 1 dog.
+
+function howManyDalmatians(number) {
+  const dogs = [
+    "Hardly any",
+    "More than a handful!",
+    "Woah that's a lot of dogs!",
+    "101 DALMATIANS!!!",
+  ];
+  if (number <= 10) return dogs[0];
+  if (number <= 50) return dogs[1];
+  if (number === 101) return dogs[3];
+  return dogs[2];
+}
