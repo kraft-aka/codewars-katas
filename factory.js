406c6c0c9308e7c8b3d5004cd9ee47450fbbaa09
kraft-aka <kubat.aiupov@gmail.com>
2024-01-24 18:20:00 +0100

    add factory

---
+++ factory.js
@@ -0,0 +1,15 @@
+// Write a function, factory, that takes a number as its parameter and returns another function.
+
+// The returned function should take an array of numbers as its parameter, and return an array of those numbers multiplied
+// by the number that was passed into the first function.
+
+// In the example below, 5 is the number passed into the first function. So it returns a function that takes an array and
+//  multiplies all elements in it by five.
+
+// Translations and comments (and upvotes) welcome!
+
+function factory(x) {
+  return function (arr) {
+    return arr.map((i) => i * x);
+  };
+}
