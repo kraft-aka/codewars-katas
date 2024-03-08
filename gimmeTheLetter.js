24d9375a19fec31de8341cfdfe8f918e2059de31
kraft-aka <kubat.aiupov@gmail.com>
2024-03-08 23:49:49 +0100

    add gimmeTheLetter

---
+++ gimmeTheLetter.js
@@ -0,0 +1,29 @@
+// Given a string indicating a range of letters, return a string which includes all the letters in that range,
+// including the last letter.
+// Note that if the range is given in capital letters, return the string in capitals also!
+
+// Examples
+// "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+// "h-o" ➞ "hijklmno"
+// "Q-Z" ➞ "QRSTUVWXYZ"
+// "J-J" ➞ "J"
+// Notes
+// A hyphen will separate the two letters in the string.
+// You don't need to worry about error handling in this kata (i.e. both letters will be the same case and the second letter will
+//     not be before the first alphabetically).
+
+function gimmeTheLetters(sp) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const a = sp[0];
+  const b = sp[2];
+  if (a == a.toUpperCase() && b == b.toUpperCase()) {
+    return letters
+      .toUpperCase()
+      .slice(
+        letters.toUpperCase().indexOf(a),
+        letters.toUpperCase().indexOf(b) + 1
+      );
+  } else {
+    return letters.slice(letters.indexOf(a), letters.indexOf(b) + 1);
+  }
+}
