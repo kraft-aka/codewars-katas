ad80e373288e640f0934332049a3e2898c4ab9e8
kubat <kubat.aiupov@gmail.com>
2024-03-11 23:21:50 +0100

    add uefaEuro2016

---
+++ uefaEuro2016.js
@@ -0,0 +1,13 @@
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+function uefaEuro2016(teams, scores) {
+  if (scores[0] > scores[1])
+    return `At match ${teams.join(" - ")}, ${teams[0]} won!`;
+  if (scores[0] < scores[1])
+    return `At match ${teams.join(" - ")}, ${teams[1]} won!`;
+  return `At match ${teams.join(" - ")}, teams played draw.`;
+}
