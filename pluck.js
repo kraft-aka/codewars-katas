8903ea66c5a3743fc97fa470594edc8cfddd8575
kubat <kubat.aiupov@gmail.com>
2024-03-14 13:13:56 +0100

    add pluck

---
+++ pluck.js
@@ -0,0 +1,15 @@
+// Don't you just love Wikipedia lists?
+
+// In this Kata, you'll write a function that extracts pieces of
+// data and stores them in an Array.
+
+// The function will accept two parameters:
+
+// data An Array of Objects
+// property A String representing the property from which to pluck data
+// An Array should be returned, containing the value extracted for
+// each Object contained in the data Array.
+
+// See the Test Cases for examples.
+
+const pluck = (data, property) => data.map((i) => i[property]);
