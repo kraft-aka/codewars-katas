fb1622f1d817f9c955ffa57ffa5ab02bc5839ceb
kubat <kubat.aiupov@gmail.com>
2024-03-15 11:50:39 +0100

    add isUndefined

---
+++ isUndefined.js
@@ -0,0 +1,5 @@
+// Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+// Task
+// Your task is to finish the isUndefined method which returns true if the value is undefined :)
+
+const isUndefined = (value) => value === undefined;
