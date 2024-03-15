9c18fbdf15f0f1b33b9fa2dd001438fcd557f125
kubat <kubat.aiupov@gmail.com>
2024-03-15 16:47:41 +0100

    add sum

---
+++ sum.js
@@ -0,0 +1,15 @@
+// Your job is to add a method 'sum' to all array objects, which sums all the numbers in it. You should return 0 if the array is empty.
+
+// For example:
+
+// var arr = [1,2,3,4];
+// arr.sum();  //returns 10
+
+// var arr = [10, 28, 14, 33];
+// arr.sum(); //returns 85
+
+Array.prototype.sum = function () {
+  let sum = 0;
+  this.forEach((i) => (sum += i));
+  return sum;
+};
