a2b5de8c0aed0f543f22f8fbea492dae0e392603
kubat <kubat.aiupov@gmail.com>
2024-04-19 23:42:10 +0200

    add addSimpleComparison

---
+++ addSimpleComparison.js
@@ -0,0 +1,5 @@
+// Write a function that will compare two values, one will be a number and one
+// will be a string. Return true if they are the same character (regardless of
+// their different data types) and return false if they are not.
+
+const add = (a, b) => a == b;
