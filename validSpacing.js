92981cd46a9ceb7322ca77ff030869ad3fa51e39
kubat <kubat.aiupov@gmail.com>
2024-04-25 23:02:04 +0200

    add validSpaing

---
+++ validSpacing.js
@@ -0,0 +1,25 @@
+// Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing.
+// The function should return either true or false (or the corresponding value in each language).
+
+// For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces.
+// Words can be any consecutive sequence of non space characters. Below are some examples of what the function should return:
+
+// * 'Hello world'   => true
+// * ' Hello world'  => false
+// * 'Hello world  ' => false
+// * 'Hello  world'  => false
+// * 'Hello'         => true
+
+// Even though there are no spaces, it is still valid because none are needed:
+// * 'Helloworld'    => true
+// * 'Helloworld '   => false
+// * ' '             => false
+// * ''              => true
+
+function validSpacing(s) {
+  if (s.length === 0) {
+    return true;
+  } else {
+    return s.split(" ").filter((i) => i == "").length < 1;
+  }
+}
