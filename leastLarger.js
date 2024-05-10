5af8c911a8123a88d2ffd2b06028472c415ecddc
kubat <kubat.aiupov@gmail.com>
2024-05-10 11:27:10 +0200

    add leastLarger

---
+++ leastLarger.js
@@ -0,0 +1,17 @@
+// Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index,
+// or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Notes
+// Multiple correct answers may be possible. In this case, return any one of them.
+// The given index will be inside the given array.
+// The given array will, therefore, never be empty.
+
+// Example
+// leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+// leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+function leastLarger(a, i) {
+  const item = a[i];
+  let filteredItems = a.filter((el) => el > item).sort((a, b) => a - b)[0];
+  return a.indexOf(filteredItems);
+}
