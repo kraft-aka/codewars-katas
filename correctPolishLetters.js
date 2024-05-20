6fd0dbf0cac0aee7ea4da3189630efbd284f9d3c
kubat <kubat.aiupov@gmail.com>
2024-05-20 23:59:28 +0200

    add correctPolishLetters

---
+++ correctPolishLetters.js
@@ -0,0 +1,44 @@
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters(string) {
+  const letters = {
+    ą: "a",
+    ć: "c",
+    ę: "e",
+    ł: "l",
+    ń: "n",
+    ó: "o",
+    ś: "s",
+    ź: "z",
+    ż: "z",
+  };
+
+  let str = "";
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (letters[char]) {
+      str += letters[char];
+    } else {
+      str += string[i];
+    }
+  }
+  return str;
+}
