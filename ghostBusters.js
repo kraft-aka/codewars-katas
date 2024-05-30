1360fe9eb74070ca31713eed187bac080f001e43
kubat <kubat.aiupov@gmail.com>
2024-05-30 17:52:58 +0200

    add ghostBuster

---
+++ ghostBusters.js
@@ -0,0 +1,21 @@
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them
+// and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings
+// represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any
+// ghosts(whitespaces)!
+
+// Example:
+
+// "Sky scra per" -> "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+function ghostBusters(building) {
+  return building.includes(" ")
+    ? building.replace(/\s/g, "")
+    : "You just wanted my autograph didn't you?";
+}
