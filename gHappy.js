e222f3408244d8c788b39f9029f4c6ea34e4d306
kubat <kubat.aiupov@gmail.com>
2024-06-24 23:22:34 +0200

    add gHappy

---
+++ gHappy.js
@@ -0,0 +1,21 @@
+// Let's say that "g" is happy in the given string, if there is another "g"
+// immediately to the right or to the left of it.
+
+// Find out if all "g"s in the given string are happy.
+
+// Example
+// For str = "gg0gg3gg0gg", the output should be true.
+
+// For str = "gog", the output should be false.
+
+// Input/Output
+// [input] string str
+// A random string of lower case letters, numbers and spaces.
+
+// [output] a boolean value
+// true if all "g"s are happy, false otherwise.
+
+function gHappy(str) {
+  let allG = str.match(/g+/g) || [];
+  return allG.every((w) => w.length > 1);
+}
