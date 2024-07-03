f13fecc62baecbdb442d09d451e77b3a7c82ce7a
kubat <kubat.aiupov@gmail.com>
2024-07-03 21:35:15 +0200

    add countFeelings

---
+++ countFeelings.js
@@ -0,0 +1,38 @@
+// You have two arguments: string - a string of random letters(only lowercase) and array - an array of strings(feelings).
+// Your task is to return how many specific feelings are in the array.
+
+// For example:
+
+// string -> 'yliausoenvjw'
+// array -> ['anger', 'awe', 'joy', 'love', 'grief']
+// output -> '3 feelings.' // 'awe', 'joy', 'love'
+
+// string -> 'griefgriefgrief'
+// array -> ['anger', 'awe', 'joy', 'love', 'grief']
+// output -> '1 feeling.' // 'grief'
+
+// string -> 'abcdkasdfvkadf'
+// array -> ['desire', 'joy', 'shame', 'longing', 'fear']
+// output -> '0 feelings.'
+// If the feeling can be formed once - plus one to the answer.
+
+// If the feeling can be formed several times from different letters - plus one to the answer.
+
+// Eeach letter in string participates in the formation of all feelings. 'angerw' -> 2 feelings: 'anger' and 'awe'.
+
+function countFeelings(string, array) {
+  const feelings = array.filter((feeling) => {
+    const regex = new RegExp(
+      `^${feeling
+        .split("")
+        .map((char) => `(?=.*${char})`)
+        .join("")}.*$`
+    );
+    return regex.test(string);
+  });
+  return feelings.length > 1
+    ? `${feelings.length} feelings.`
+    : feelings.length == 1
+    ? `${feelings.length} feeling.`
+    : "0 feelings.";
+}
