b6fd04d7fa97f4736e48afc2b4a0993bc9b7a44a
kubat <kubat.aiupov@gmail.com>
2024-07-29 21:59:06 +0200

    add broken

---
+++ broken.js
@@ -0,0 +1,17 @@
+// The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+// Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+// Given a string of binary, return the version the photocopier gives you as a string.
+
+function broken(x) {
+  let res = "";
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] == "1") {
+      res += "0";
+    } else {
+      res += "1";
+    }
+  }
+  return res;
+}
