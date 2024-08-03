a72145c0f495cd564c0c96568ac5a238efd64736
kubat <kubat.aiupov@gmail.com>
2024-08-03 20:31:47 +0200

    add maxProduct

---
+++ maxProduct.js
@@ -0,0 +1,29 @@
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+
+function maxProduct(numbers, size) {
+  let products = 1;
+
+  numbers.sort((a, b) => b - a);
+
+  for (let i = 0; i < size; i++) {
+    products *= numbers[i];
+  }
+
+  return products;
+}
