31a16108ad5b85a1d427dde73e7fe7730d21ae25
kraft-aka <kubat.aiupov@gmail.com>
2024-08-14 21:57:26 +0200

    add isSameLanguage

---
+++ isSameLanguage.js
@@ -0,0 +1,21 @@
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// your function should return true.
+
+
+function isSameLanguage(list) {
+    let arr = [];
+    list.forEach(dev => arr.push(dev.language));
+    return new Set(arr).size === 1;
+}
