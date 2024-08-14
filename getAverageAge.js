c56fae22776ca8f8321f1c556d4b1bcb6c388f81
kraft-aka <kubat.aiupov@gmail.com>
2024-08-14 22:08:51 +0200

    add getAverageAge

---
+++ getAverageAge.js
@@ -0,0 +1,12 @@
+// You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+// ];
+// write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+
+const getAverageAge = (list) => Math.round(list.reduce((a, b) => a + b.age, 0) / list.length);
