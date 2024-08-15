2e7c85fdceda6e6e79c1862e74489481f733a64b
kraft-aka <kubat.aiupov@gmail.com>
2024-08-15 22:35:36 +0200

    add arrayLowerCase

---
+++ arrayLowerCase.js
@@ -0,0 +1,10 @@
+// Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+
+// Example
+// arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+// arrayLowerCase([1, 'Green']) == [1, 'green']
+
+
+function arrayLowerCase(arr) {
+    return arr.map(i => typeof i === 'string' ? i.toLowerCase() : i)
+}
