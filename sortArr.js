40123830e8682f7728bdd60614c56f9120df0cf9
kubat <kubat.aiupov@gmail.com>
2024-10-10 21:50:35 +0200

    add sortArr

---
+++ sortArr.js
@@ -0,0 +1,8 @@
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+const sortme = (names) => {
+  return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+};
