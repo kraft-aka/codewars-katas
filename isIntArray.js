347ecbacc4b4feea463c61b39902273b9ec31d8f
kubat <kubat.aiupov@gmail.com>
2024-10-10 22:03:28 +0200

    add isIntArray

---
+++ isIntArray.js
@@ -0,0 +1,19 @@
+// Write a function with the signature shown below:
+
+// function isIntArray(arr) {
+//   return true
+// }
+// returns true  / True if every element in an array is an integer or a float with no decimals.
+// returns true  / True if array is empty.
+// returns false / False for every other input.
+
+function isIntArray(arr) {
+  if (!arr) {
+    return false;
+  }
+  return (
+    arr.every((i) => Number.isInteger(i) && !isNaN(i)) ||
+    arr.length === 0 ||
+    false
+  );
+}
