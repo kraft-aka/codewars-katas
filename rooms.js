1783a6673042b1414ae377178b956907aede96af
kubat <kubat.aiupov@gmail.com>
2024-12-09 22:37:08 +0100

    add rooms

---
+++ rooms.js
@@ -0,0 +1,17 @@
+var rooms = {
+  roomOne: {
+    name: "xxx",
+    description: "xxx",
+    completed: false,
+  },
+  roomTwo: {
+    name: "yyy",
+    description: "yyy",
+    completed: false,
+  },
+  roomThree: {
+    name: "zzz",
+    description: "zzz",
+    completed: false,
+  },
+};
