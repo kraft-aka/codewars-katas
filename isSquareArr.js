9009a66987febcdef40564da20492a868d0f5623
kubat <kubat.aiupov@gmail.com>
2025-02-04 21:13:24 +0100

    add isSquareArr

---
+++ isSquareArr.js
@@ -0,0 +1,17 @@
+// Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+// Your function should return true if all elements in the array are square numbers and false if not.
+
+// An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+// Examples:
+
+// isSquare([1, 4, 9, 16]) --> true
+
+// isSquare([3, 4, 7, 9]) --> false
+
+// isSquare([]) --> undefined
+
+var isSquare = function (arr) {
+  return arr.length ? arr.every((i) => i === Math.sqrt(i) ** 2) : undefined;
+};
