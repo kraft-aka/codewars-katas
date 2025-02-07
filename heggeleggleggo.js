06cf9331b68a94181deb94db060625c7355daf6d
kraft-aka <kubat.aiupov@gmail.com>
2025-02-07 22:56:41 +0100

    add heggeleggleggo

---
+++ heggeleggleggo.js
@@ -0,0 +1,17 @@
+// Egg Talk.
+
+// Insert an egg after each consonant. If there are no consonants, there will be no eggs. Argument will consist of a string with only alphabetic characters and possibly some spaces.
+
+// Example
+// hello => heggeleggleggo
+
+// eggs => egegggeggsegg
+
+// FUN KATA => FeggUNegg KeggATeggA
+
+function heggeleggleggo(word) {
+  return word
+    .split(" ")
+    .map((w) => w.replace(/([^aeiou])/gi, "$1egg"))
+    .join(" ");
+}
