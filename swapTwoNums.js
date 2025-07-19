ace423e8f93ba9017c8472ae875ab533fc007e06
kraft-aka <kubat.aiupov@gmail.com>
2025-07-19 12:53:03 +0200

    add swapTwoNums

---
+++ swapTwoNums.js
@@ -0,0 +1,12 @@
+// Task
+// Write a swap function to swap 2 integers in place
+
+// NOTES
+
+// The input parameter is an integer array with 2 elements. ary.length == 2
+// You are not allowed to rename the input parameter
+// You are not allowed to use any other variables
+// You are not allowed to use any other functions
+// You are not allowed to use any other keywords
+
+swap = (ary) => ([ary[0], ary[1]] = [ary[1], ary[0]]);
