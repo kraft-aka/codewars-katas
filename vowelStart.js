5aff6ca46a3d18cecb2eb759012b8e5278a97175
kubat <kubat.aiupov@gmail.com>
2025-08-20 21:58:54 +0200

    add vowelStart

---
+++ vowelStart.js
@@ -0,0 +1,21 @@
+// Start with a Vowel
+// Create a function that takes any sentence and redistributes the spaces (and adds additional spaces if needed) so that each word starts with a vowel. The letters should all be in the same order but every vowel in the sentence should be the start of a new word. The first word in the new sentence may start without a vowel. It should return a string in all lowercase with no punctuation (only alphanumeric characters).
+// 'It is beautiful weather today!' => 'it isb e a ut if ulw e ath ert od ay'
+// 'Coding is great' => 'c od ing isgr e at'
+// 'my number is 0208-533-2325' => 'myn umb er is02085332325'
+
+function vowelStart(str) {
+  const vowels = "aeoui".split("");
+  return str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split(" ")
+    .map((word) =>
+      word
+        .split("")
+        .map((i) => (vowels.includes(i) ? " " + i : i))
+        .join("")
+    )
+    .join("")
+    .trimStart();
+}
