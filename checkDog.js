1d0e93bc8e11213b1309e3b13f2f14130b8a1184
kraft-aka <kubat.aiupov@gmail.com>
2025-09-12 22:56:09 +0200

    add checkDog

---
+++ checkDog.js
@@ -0,0 +1,13 @@
+// Doggy Daycare
+
+Dog.prototype.checkDog = function(){
+  if (this.vaccinated && this.wormed) {
+    return `${this.name} can be accepted`
+  }
+  else if (this.vaccinated || this.wormed) {
+    return `${this.name} can only be accepted by itself`
+  } 
+  else {
+    return `${this.name} can not be accepted`
+  }
+}
