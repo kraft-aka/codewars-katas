3b6c9a45cabf26e2cc0dcb5c2b9a3813fca19184
kubat <kubat.aiupov@gmail.com>
2025-09-14 06:47:09 +0200

    add prevMultOfThree

---
+++ prevMultOfThree.js
@@ -0,0 +1,21 @@
+// Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+
+// Return n if the input is already a multiple of three, and if no such number exists, return null, a similar empty value, or -1.
+
+// Examples
+// 1      => null
+// 25     => null
+// 36     => 36
+// 1244   => 12
+// 952406 => 9
+
+const prevMultOfThree = (n) => {
+  if (n <= 0) return null;
+
+  while (n > 0) {
+    if (n % 3 === 0) return n;
+    n = Math.floor(n / 10);
+  }
+
+  return null;
+};
