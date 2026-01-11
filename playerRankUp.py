bb9a077485be4f352ed7120f69574aa2acbaedd4
kraft-aka <kubat.aiupov@gmail.com>
2026-01-11 19:42:13 +0100

    add playerRankUp

---
+++ playerRankUp.py
@@ -0,0 +1,7 @@
+# Online RPG: player to qualifying stage?
+
+def player_rank_up(pts):
+     if pts >= 100:
+            return 'Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.'
+     else: 
+            return False
